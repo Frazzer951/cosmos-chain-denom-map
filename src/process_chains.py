ae7b8fea62be7bd7f7a5c08f3a040900516d298f
Luke Eltiste <luke@sonarverse.com>
2023-10-27 11:46:49 -0700

    refactor

--- src/process_chains.py
+++ src/process_chains.py
@@ -12,67 +12,78 @@ logger = setup_logger('process_chains')
 
 
 def export_file(file_name, data, min_file=False):
-    logger.info("Writing denom_map.json")
     with open(f"{file_name}.json", "w") as f:
-        f.write(json.dumps(data, indent=4, sort_keys=True))
-    if min_file:
-        logger.info("Writing denom_map_min.json")
-        with open(f"{file_name}_min.json", "w") as f:
-            f.write(json.dumps(data, sort_keys=True))
+        json.dump(data, f, indent=None if min_file else 4, sort_keys=True)
+
+
+def process_assets_for_chain(chain: Chain):
+    denoms_for_chain = {}
+
+    for asset in tqdm(chain.assets, desc=f"Processing assets for {chain.chain_name}", leave=False):
+        denoms = {denom_unit.denom.lower(): denom_unit for denom_unit in asset.denom_units}
+        display_denom = denoms.get(asset.display.lower())
+
+        # If asset.display.lower() didn't match a denom, check aliases
+        if not display_denom:
+            for denom_unit in asset.denom_units:
+                if denom_unit.aliases:
+                    if asset.display.lower() in [alias.lower() for alias in denom_unit.aliases]:
+                        logger.info(f'Found alias {asset.display} for {asset.name} on {chain.chain_name}')
+                        display_denom = denom_unit
+                        break
+
+        if not display_denom:
+            logger.error(
+                f"Display denom {asset.display} not found in denom_units for {asset.name} on {chain.chain_name}")
+            continue
+
+        display_decimals = display_denom.exponent
+        for denom in denoms:
+            denom_key = f'{denom}_{chain.chain_name}'
+            if denom_key in denoms_for_chain:
+                logger.warning(f"Denom {denom_key} already exists for the chain {chain.chain_name}")
+                continue
+
+            decimals = display_decimals - denoms[denom].exponent
+            denoms_for_chain[denom_key] = Denom(
+                denom=denoms[denom].denom,
+                name=asset.name,
+                symbol=asset.symbol,
+                decimals=decimals,
+                chain=chain.chain_name,
+                description=asset.description,
+                aliases=denoms[denom].aliases,
+                type_asset=asset.type_asset,
+                address=asset.address,
+                traces=asset.traces,
+                ibc=asset.ibc,
+                logo_URIs=asset.logo_URIs,
+                images=asset.images,
+                coingecko_id=asset.coingecko_id,
+                keywords=asset.keywords,
+            )
+
+    return denoms_for_chain
 
 
 def process_chains(force_update=False):
-    if force_update:
-        # Delete commit_id.json if it exists to force an update
-        if os.path.exists("commit_id.json"):
-            os.remove("commit_id.json")
+    if force_update and os.path.exists("commit_id.json"):
+        os.remove("commit_id.json")
 
-    new_changes = download_chain_registry()
+    if not download_chain_registry():
+        logger.info("No new changes to process.")
+        return
 
-    if new_changes:
-        denom_map = DenomMap(denoms={})
-        chains_raw = load_assets()
-        chains = [Chain(**chain_data) for chain_data in chains_raw]
+    denom_map = DenomMap(denoms={})
+    chains = [Chain(**chain_data) for chain_data in load_assets()]
 
-        for chain in tqdm(chains, desc="Processing chains"):
-            for asset in tqdm(chain.assets, desc=f"Processing assets for {chain.chain_name}", leave=False):
-                denoms = {denom_unit.denom.lower(): denom_unit for denom_unit in asset.denom_units}
-                base_denom = denoms.get(asset.base.lower())
-                if not base_denom:
-                    logger.error(f"Base denom {asset.base} not found in denom_units for {asset.name}")
-                    continue
+    for chain in tqdm(chains, desc="Processing chains"):
+        denom_map.denoms.update(process_assets_for_chain(chain))
 
-                display_decimals = base_denom.exponent
-                for denom in denoms:
-                    denom_key = f'{denom}_{chain.chain_name}'
-                    if denom_key in denom_map:
-                        logger.warning(f"Denom {denom_key} already exists in denom_map")
-                        continue
-
-                    decimals = display_decimals - denoms[denom].exponent
-                    denom_map.denoms[denom_key] = Denom(
-                        denom=denoms[denom].denom,
-                        name=asset.name,
-                        symbol=asset.symbol,
-                        decimals=decimals,
-                        chain=chain.chain_name,
-                        description=asset.description,
-                        aliases=denoms[denom].aliases,
-                        type_asset=asset.type_asset,
-                        address=asset.address,
-                        traces=asset.traces,
-                        ibc=asset.ibc,
-                        logo_URIs=asset.logo_URIs,
-                        images=asset.images,
-                        coingecko_id=asset.coingecko_id,
-                        keywords=asset.keywords,
-                    )
-
-        logger.info(f'Found {len(denom_map.denoms)} denoms')
-
-        # Write denoms to file
-        denom_map_dict = denom_map.model_dump()
-        export_file('denom_map', denom_map_dict['denoms'], True)
+    logger.info(f'Found {len(denom_map.denoms)} denoms')
+    denom_map_dict = denom_map.model_dump()
+    export_file('denom_map', denom_map_dict['denoms'], False)
+    export_file('denom_map_min', denom_map_dict['denoms'], True)
 
 
 if __name__ == "__main__":
