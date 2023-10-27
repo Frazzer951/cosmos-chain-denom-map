73e6161509cc5e92b8c267f3d4be9fa206bdb5d7
Luke Eltiste <luke@sonarverse.com>
2023-10-27 10:28:14 -0700

    load files

---
+++ src/load_asset_files.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+
+def find_asset_files(root_dir, exclude_dirs=[]):
+    asset_files = []
+
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        # remove excluded directories
+        dirnames[:] = [d for d in dirnames if d not in exclude_dirs]
+
+        for filename in filenames:
+            if filename == "assetlist.json":
+                asset_files.append(os.path.join(dirpath, filename))
+
+    return asset_files
+
+
+def load_json_files(file_paths):
+    loaded_files = []
+
+    for file_path in file_paths:
+        with open(file_path, 'r') as f:
+            loaded_files.append(json.load(f))
+
+    return loaded_files
+
+
+def load_assets():
+    root_directory = "./chain-registry"
+    exclude_directories = [".git", ".github", "_template"]
+
+    asset_file_paths = find_asset_files(root_directory, exclude_directories)
+    loaded_assets = load_json_files(asset_file_paths)
+
+    print(f'Loaded {len(loaded_assets)} assets')
+    return loaded_assets
+
+
+if __name__ == "__main__":
+    load_assets()
